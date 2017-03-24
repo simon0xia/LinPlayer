0f5b3ba865a24abf64a0ab2372534baab7f5d91a
simon0xia <xie.yong_hen@hotmail.com>
2017-03-24 16:38:31 +0800

    记忆上次播放文件的路径；动态切换play、sound等按钮状态；

--- LinPlayer/linplayer.cpp
+++ LinPlayer/linplayer.cpp
@@ -1,7 +1,7 @@
 #include "linplayer.h"
 #include <QMessageBox>
+#include <QSettings>
 #include "Log.h"
-
 
 LinPlayer::LinPlayer(QWidget *parent)
 : QMainWindow(parent), player(NULL)
@@ -10,13 +10,15 @@ LinPlayer::LinPlayer(QWidget *parent)
 
 	LogIns.Init("LinPlayer.log");
 	CPlayCore::InitEnv();
+	ReadSettings();
 
 	ui.btn_Stop->setDisabled(true);
 	ui.btn_Previous->setDisabled(true);
 	ui.btn_Next->setDisabled(true);
+	ui.btn_boost->setDisabled(true);
+	ui.btn_reduce->setDisabled(true);
 	
 	connect(ui.btn_Open, SIGNAL(clicked()), this, SLOT(ctrl_open()));
-
 	connect(ui.btn_Play, SIGNAL(clicked()), this, SLOT(ctrl_play()));
 	connect(ui.btn_Stop, SIGNAL(clicked()), this, SLOT(ctrl_stop()));
 	connect(ui.btn_Previous, SIGNAL(clicked()), this, SLOT(ctrl_previous()));
@@ -48,51 +50,93 @@ void LinPlayer::DisableAllButton()
 
 void LinPlayer::ctrl_open(void)
 {
+	//先停止当前正在播放内容，再弹出对话框供用户选择播放文件。
 	ctrl_stop();
 
-	QString filter =tr( "" );
-	QString selFilter = tr("视频文件 (*.avi *.h264);;媒体文件（所有文件）(*.*)");
-	strUrl = QFileDialog::getOpenFileName(this, "Select Media File", NULL, selFilter);
+	//打开”选择文件“对话框时，直接切换到上次播放所在的路径。
+	QString strPath = strUrl;
+	if (!strPath.isEmpty())	{
+		
+		int index = strPath.lastIndexOf("/");
+		strPath.truncate(index);
+	}
+
+	QString title = QStringLiteral("选择文件");
+	QString selFilter = QStringLiteral("视频文件 (*.avi *.h264);;媒体文件（所有文件）(*.*)");
+	strUrl = QFileDialog::getOpenFileName(this, title, strPath, selFilter);
 
 	ctrl_play();
 }
 
 void LinPlayer::ctrl_play(void)
 {
-	if (player != nullptr && player->isPlaying())
+	if (player == nullptr)
 	{
-		player->pause();
-		setStyleSheet_btn_play();
-		return;
+		//如果没有播放，则生成播放器，并播放strUrl指向的多媒体体文件。	
+		ui.graphicsView->setVisible(true);
+		if (!strUrl.isEmpty())
+		{
+			player = new CPlayCore((HWND)ui.graphicsView->winId());
+			player->play(strUrl.toStdString().c_str());
+
+			setStyleSheet_btn_play(player->isPause());
+			ui.btn_Stop->setDisabled(false);
+		}
 	}
-
-	if (!strUrl.isEmpty())
+	else //if (player->isPlaying())
 	{
-		player = new CPlayCore((HWND)ui.graphicsView->winId());
-		player->play(strUrl.toStdString().c_str());
-
-		setStyleSheet_btn_play();
-		ui.btn_Stop->setDisabled(false);
+		//如果正在播放，则暂停播放，并切换‘播放’钮钮的状态。
+		player->pause();
+		setStyleSheet_btn_play(player->isPause());
+		return;
 	}
 }
 	
-void LinPlayer::setStyleSheet_btn_play()
+void LinPlayer::setStyleSheet_btn_play(bool bPause)
 {
-	if (player->isPause())
+	if (bPause)
 	{
-		ui.btn_Play->setStyleSheet("QPushButton{\n    border:none;\n    border-image: url(:/LinPlayer/Resources/button.png)0 704 192 64;\n}\nQPushButton:hover{\n   border-image: url(:/LinPlayer/Resources/button.png) 64 704 128 64;\n}\nQPushButton:pressed{\n   border-image: url(:/LinPlayer/Resources/button.png) 128 704 64 64;\n}\nQPushButton:disabled{\n   border-image: url(:/LinPlayer/Resources/button.png) 192 704 0 64;\n}");
+		ui.btn_Play->setStyleSheet("QPushButton{ border:none; border-image: url(:/LinPlayer/Resources/button.png)0 768 192 0;} "
+								"QPushButton:hover   { border-image: url(:/LinPlayer/Resources/button.png) 64 768 128 0;} "
+								"QPushButton:pressed { border-image: url(:/LinPlayer/Resources/button.png) 128 768 64 0;}"
+								"QPushButton:disabled{ border-image: url(:/LinPlayer/Resources/button.png) 192 768 0 0;}");
 	}
 	else
 	{
-		ui.btn_Play->setStyleSheet("QPushButton{\n    border:none;\n    border-image: url(:/LinPlayer/Resources/button.png)0 768 192 0;\n}\nQPushButton:hover{\n   border-image: url(:/LinPlayer/Resources/button.png) 64 768 128 0;\n}\nQPushButton:pressed{\n   border-image: url(:/LinPlayer/Resources/button.png) 128 768 64 0;\n}\nQPushButton:disabled{\n   border-image: url(:/LinPlayer/Resources/button.png) 192 768 0 0;\n}");
+		ui.btn_Play->setStyleSheet("QPushButton{ border:none; border-image: url(:/LinPlayer/Resources/button.png)0 704 192 64;} "
+								"QPushButton:hover   { border-image: url(:/LinPlayer/Resources/button.png) 64 704 128 64;}"
+								"QPushButton:pressed { border-image: url(:/LinPlayer/Resources/button.png) 128 704 64 64;}"
+								"QPushButton:disabled{ border-image: url(:/LinPlayer/Resources/button.png) 192 704 0 64;}");
+	}
+}
+
+void LinPlayer::setStyleSheet_btn_Sound(bool bMute)
+{
+	if (bMute)
+	{
+		ui.btn_Sound->setStyleSheet("QPushButton{ border:none; border-image: url(:/LinPlayer/Resources/button.png)0 256 192 512;} "
+								"QPushButton:hover   { border-image: url(:/LinPlayer/Resources/button.png) 64 256 128 512;}"
+								"QPushButton:pressed { border-image: url(:/LinPlayer/Resources/button.png) 128 256 64 512;}"
+								"QPushButton:disabled{ border-image: url(:/LinPlayer/Resources/button.png) 192 256 0 512;}");
+	}
+	else
+	{
+		ui.btn_Sound->setStyleSheet("QPushButton{ border:none; border-image: url(:/LinPlayer/Resources/button.png)0 192 192 512;} "
+								"QPushButton:hover   { border-image: url(:/LinPlayer/Resources/button.png) 64 192 128 512;}"
+								"QPushButton:pressed { border-image: url(:/LinPlayer/Resources/button.png) 128 192 64 512;}"
+								"QPushButton:disabled{ border-image: url(:/LinPlayer/Resources/button.png) 192 192 0 512;}");
 	}
 }
 
 void LinPlayer::ctrl_stop(void)
 {
-	if (player != NULL)
+	if (player != nullptr)
 	{
 		player->stop();
+
+		delete player;
+		player = nullptr;
+
 	}
 	
 	ui.btn_Stop->setDisabled(true);
@@ -122,5 +166,13 @@ void LinPlayer::ctrl_fullscreen(void)
 
 void LinPlayer::ctrl_sound(void)
 {
+	player->setMute(!player->isMute());
+	setStyleSheet_btn_Sound(player->isMute());
+}
 
+void LinPlayer::ReadSettings()
+{
+	QSettings set("./setting.ini", QSettings::IniFormat);
+	
+	strUrl = set.value("comm/preMedia", "").toString();
 }
